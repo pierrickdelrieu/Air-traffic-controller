719a7226c9d876043a427f4147ea7c94fe7dffd5
“pierrickdelrieugit config --global user.email “pierrick.delrieu@efrei.net <pierrick.delrieu@efrei.net>
2022-03-11 17:39:36 -0500

    project architecture

--- src/Element/Obstacle.java
+++ src/Element/Obstacle.java
@@ -1,5 +1,30 @@
 package Element;
 
 public class Obstacle extends Element {
+    public static enum ObstacleType {PROTESTOR};
+
+    private final int time;
+    private final ObstacleType type;
+
+    public Obstacle(ObstacleType type, int time) {
+        this.type = type;
+        this.time = time;
+    }
+
+    public int getTime() {
+        return this.time;
+    }
+
+    public ObstacleType getType() {
+        return this.type;
+    }
+
+    @Override
+    public String toString() {
+        if (this.type == ObstacleType.PROTESTOR) {
+            return "M";
+        }
+        return "";
+    }
 
 }
