1d0737b1db13434f55dec5a6871f24e1896292b2
“pierrickdelrieugit config --global user.email “pierrick.delrieu@efrei.net <pierrick.delrieu@efrei.net>
2022-03-13 19:40:19 -0400

    fic error

--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
+import AiTravelController.UnitTest.OptionsUnitTest;
+
 public class main {
     public static void main(String[] args) {
         AiTravelController.AirTravelController.getInstance().run();
+
+//        System.out.println(OptionsUnitTest.AllowLanding());
     }
 }
