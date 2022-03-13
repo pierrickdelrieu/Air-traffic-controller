d280b40f9ccf01b62a7b1cf8fdbdfb447a1d0093
“pierrickdelrieugit config --global user.email “pierrick.delrieu@efrei.net <pierrick.delrieu@efrei.net>
2022-03-13 19:37:39 -0400

    add unit test file

---
+++ src/AiTravelController/UnitTest/OptionsUnitTest.java
@@ -0,0 +1,16 @@
+package AiTravelController.UnitTest;
+
+import AiTravelController.Element.Plane;
+import AiTravelController.RunwayWaiting.RunwayController;
+import AiTravelController.RunwayWaiting.WaitingPlaneController;
+
+public class OptionsUnitTest {
+
+    public static boolean AllowLanding() {
+//        WaitingPlaneController.addPlane(new Plane("test", 4, 4, 4));
+//        AllowLanding()
+
+
+        return true;
+    }
+}
