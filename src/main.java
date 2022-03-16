0773e70d67d0f70da42688b2e24feb9501aff30c
Guipas68 <58084848+Guipas68@users.noreply.github.com>
2022-03-16 15:21:22 -0400

    Update main.java

--- src/main.java
+++ src/main.java
@@ -5,26 +5,8 @@ public class main {
 //        AiTravelController.AirTravelController.getInstance().run();
 
 
-        // Options test
-
-
-        //System.out.println(OptionsUnitTest.AirRefuelingTest());
-        //System.out.println(OptionsUnitTest.AllowLandingTest());
-        //System.out.println(OptionsUnitTest.CrossWindsTest());
-        System.out.println(OptionsUnitTest.EmptyRandomRunwayTest());
-        //System.out.println(OptionsUnitTest.GetPoliceInvolvedTest());
-
-        //System.out.println(OptionsUnitTest.IceStormTest());
-        //System.out.println(OptionsUnitTest.IcyRunwaysTest());
-        //System.out.println(OptionsUnitTest.LetTheManGoTest());
-        //System.out.println(OptionsUnitTest.LetThemLandTest());
-        //System.out.println(OptionsUnitTest.LockdownTheRunwayTest());
-
-        //System.out.println(OptionsUnitTest.PlaceOnStandbyTest());
-        //System.out.println(OptionsUnitTest.PlanesLandAnywaysTest());
-        //System.out.println(OptionsUnitTest.RefuseThemTest());
-        //System.out.println(OptionsUnitTest.RescueTeamTest());
-        //System.out.println(OptionsUnitTest.SpaceToProtestTest());
+        // Options Unit tests
+        System.out.println(OptionsUnitTest.MainOptionsUnitTest());
 
     }
 }
